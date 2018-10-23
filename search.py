84a5fbcd7cba0e26e066a223f956573eded0281c
Uğur Kubilay ÇAM <32200607+ndorte@users.noreply.github.com>
2018-10-23 17:13:17 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,52 +1,56 @@
-import requests, random, time
+import requests, random, time, os
 from bs4 import BeautifulSoup
 
-def google_ara(kelime, hedefsite):
-    with open("user-agents-desktop.txt", "r") as agents:
-        agent = random.choice(agents.readlines())
-        a = agent.strip()
-        header = {
-            "User-Agent": str(a)
-        }
-        agents.close()
-    kelimeduzelt = kelime.replace(" ", "+")
-    google_url = ("https://www.google.com.tr/search?q="+kelimeduzelt+"&num=100")
-    print(agent)
-    sonuc = requests.get(google_url, headers=header)
-    soup = BeautifulSoup(sonuc.text, "html.parser")
-    sonuc_blok = soup.find_all('div', attrs={'class': 'g'})
-    x = 1
-    for i in sonuc_blok:
-        cite = i.find('cite')
-        cit = str(cite)
-        if hedefsite in cit:
-            print(hedefsite, str(x), "sırada.")
-            break
-        else:
-            x += 1
-def google_ara_mobile(kelime, hedefsite):
-    with open("user-agents-mobile.txt", "r") as agents:
-        agent = random.choice(agents.readlines())
-        a = agent.strip()
-        header = {
-            "User-Agent": str(a)
-        }
-        agents.close()
-    kelimeduzelt = kelime.replace(" ", "+")
-    google_url = ("https://www.google.com.tr/search?q="+kelimeduzelt+"&num=100")
-    print(agent)
-    sonuc = requests.get(google_url, headers=header)
-    soup = BeautifulSoup(sonuc.text, "html.parser")
-    sonuc_block = soup.find_all('span', attrs={'class': 'qzEoUe'})
-    x = 1
-    for i in sonuc_block:
-        link = str(i.text)
-        if hedefsite in link:
-            print(hedefsite, str(x), "sırada.")
-            break
+class Ara:
+    def desktop(self, kelime, hedefsite):
+        ua_dir = (os.getcwd() + "/user-agents-desktop.txt")
+        with open(ua_dir, "r") as agents:
+            agent = random.choice(agents.readlines())
+            a = agent.strip()
+            header = {
+                "User-Agent": str(a)
+            }
+            agents.close()
+        kelimeduzelt = kelime.replace(" ", "+")
+        google_url = ("https://www.google.com.tr/search?q="+kelimeduzelt+"&num=100")
+        sonuc = requests.get(google_url, headers=header)
+        soup = BeautifulSoup(sonuc.text, "html.parser")
+        sonuc_blok = soup.find_all('div', attrs={'class': 'g'})
+        x = 1
+        for i in sonuc_blok:
+            cite = i.find('cite')
+            cit = str(cite)
+            if hedefsite in cit:
+                print(hedefsite, str(x), "sırada.")
+                break
+            else:
+                x += 1
+    def mobile(self, kelime, hedefsite):
+        ua_dir = (os.getcwd() + "/user-agents-desktop.txt")
+        with open(ua_dir, "r") as agents:
+            agent = random.choice(agents.readlines())
+            a = agent.strip()
+            header = {
+                "User-Agent": str(a)
+            }
+            agents.close()
+        kelimeduzelt = kelime.replace(" ", "+")
+        google_url = ("https://www.google.com.tr/search?q="+kelimeduzelt+"&num=100")
+        sonuc = requests.get(google_url, headers=header)
+        soup = BeautifulSoup(sonuc.text, "html.parser")
+        sonuc_block = soup.find_all('span', attrs={'class': 'qzEoUe'})
+        x = 1
+        for i in sonuc_block:
+            link = str(i.text)
+            if hedefsite in link:
+                print(hedefsite, str(x), "sırada.")
+                break
 
-        else:
-            x += 1
+            else:
+                x += 1
 
-google_ara("kelime", "site")
-google_ara_mobile("kelime", "site")
+p = Ara()
+p.desktop("kelime", "site")
+p.mobile("kelime", "site")
+
+
